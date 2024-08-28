ea4fd14d63222d655f7a03fb1ce42549442e0302
sxclij <sxclij@gmail.com>
2024-08-28 15:08:11 +0000

    termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{stdin, stdout, Write};
+use termion::raw::IntoRawMode;
+use termion::input::TermRead;
+use termion::event::Key;
+
 fn main() {
-    println!("good morning world.");
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    let stdin = stdin();
+    let mut keys = stdin.keys();
+
+    stdout.flush().unwrap();
+
+    loop {
+        print!("{}", "\x1b[1;1H");
+        if let Some(Ok(key)) = keys.next() {
+            match key {
+                Key::Char(c) => {
+                    print!("{}", c);
+                    stdout.flush().unwrap();
+                },
+                Key::Esc => {
+                    break;
+                },
+                _ => {}
+            }
+        }
+    }
 }
