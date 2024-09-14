1ddf3837f57eb6c2f2f2e211651fa97d50ad7a86
sxclij <sxclij@gmail.com>
2024-09-14 08:01:47 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
-use std::cell::RefCell;
-use std::collections::linked_list;
 use std::io::{stdin, stdout, Write};
-use std::rc::{Rc, Weak};
-use termion::cursor;
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;struct Node {
-    value: char,
-    next: Option<Rc<RefCell<Node>>>,
-    prev: Option<Weak<RefCell<Node>>>,
-}
-
+use termion::raw::IntoRawMode;
 
 fn main() {
     let stdin = stdin();
@@ -21,10 +12,8 @@ fn main() {
     loop {
         if let Some(Ok(key)) = keys.next() {
             match key {
-                Key::Char(c) => {
-                }
-                Key::Backspace => {
-                }
+                Key::Char(c) => {}
+                Key::Backspace => {}
                 Key::Esc => {
                     break;
                 }
